946c45746e5e6e6b9f80c193fba98b750405d174
Ahmad Maksousa <ah_maksousa@hotmail.com>
2023-01-28 05:54:06 +0300

    inhance color

--- src/Contexts/BaseThemeContext/BaseTheme/palette.js
+++ src/Contexts/BaseThemeContext/BaseTheme/palette.js
@@ -1,7 +1,7 @@
 import { amber } from "@mui/material/colors";
 
 export const composeThemePalette = ({ palette: { mode, grey } }) => ({
-  ...(mode === "light" && { background: { default: grey[200] } }),
+  ...(mode === "light" && { background: { default: grey[100] } }),
 });
 
 export const palette = {
