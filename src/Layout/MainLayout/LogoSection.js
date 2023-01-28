7b285fcbd097a6b0dae9b0617f584b7a8785a9de
Ahmad Maksousa <ah_maksousa@hotmail.com>
2023-01-28 15:47:09 +0300

    inhance text

--- src/Layout/MainLayout/LogoSection.js
+++ src/Layout/MainLayout/LogoSection.js
@@ -1,7 +1,12 @@
 import { ModeOfTravelTwoTone } from "@mui/icons-material";
-import { Avatar, ButtonBase, Typography } from "@mui/material";
+import { Avatar, ButtonBase, styled, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { useNavigate } from "react-router-dom";
+
+const LogoText = styled(Typography)(({ theme, color }) => ({
+  color: color === "primary" ? theme.palette.primary.main : theme.palette.primary.secondary,
+  fontWeight: "bold",
+}));
 
 const LogoSection = () => {
   const navigate = useNavigate();
@@ -12,14 +17,10 @@ const LogoSection = () => {
     <Box sx={{ flexGrow: 1 }}>
       <ButtonBase onClick={handleNavigate}>
         <Avatar variant="rounded" sx={{ bgcolor: (theme) => theme.palette.secondary.main, marginInlineEnd: 1 }}>
-          <ModeOfTravelTwoTone  />
+          <ModeOfTravelTwoTone />
         </Avatar>
-        <Typography variant="h5" sx={{ color: (theme) => theme.palette.primary.main, fontWeight: "bold" }}>
-          Travel
-        </Typography>
-        <Typography variant="h5" sx={{ color: (theme) => theme.palette.secondary.main }}>
-          Planner
-        </Typography>
+        <LogoText variant="h5" color="primary">Travel</LogoText>
+        <LogoText variant="h5" color="secondary">Planner</LogoText>
       </ButtonBase>
     </Box>
   );
